e427a60c550d35cf09dec79c34cef8eafb12f6d3
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-18 16:58:55 +0100

    variables in java

--- src/Variables.java
+++ src/Variables.java
@@ -1,5 +1,10 @@
 public class Variables {
     public static void main(String[] args){
         // type variable name = value;
+        int num = 20;
+        String name = "Jessica Oyalhilome. I am ";
+        String others = "My name is ";
+
+        System.out.println("");
     }
 }
