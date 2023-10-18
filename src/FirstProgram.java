a916a3e46c5374244fa6a876e781b92fe7fb6404
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-18 16:39:42 +0100

    first program

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -3,9 +3,14 @@ public class FirstProgram {
         //single line comment
         /* multi-line comment */
         System.out.println("Another java program incoming...");
+
+        //looking at the difference between string
         String str1 = new String("jess");
         String str2 = "jess";
 
+        //this is because new String creates a new storage location for that variable
+        //if you have another string with the same value String str3="jess", it will be == t str1 because both are pointing to
+        //the same memory location unlike new String which is creating a new location afresh.
         System.out.println(str1.equals(str2));
         System.out.println(str1 == str2);
     }
