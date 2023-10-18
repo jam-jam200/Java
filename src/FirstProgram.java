84a35dec94bdc57e99c7505cba07e952e6856536
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-18 16:33:08 +0100

    first program

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -3,5 +3,10 @@ public class FirstProgram {
         //single line comment
         /* multi-line comment */
         System.out.println("Another java program incoming...");
+        String str1 = new String("jess");
+        String str2 = "jess";
+
+        System.out.println(str1.equals(str2));
+        System.out.println(str1 == str2);
     }
 }
