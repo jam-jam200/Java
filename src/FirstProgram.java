f5612eaac9ada758a5f84078f0711752ad6ad97b
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-18 16:20:31 +0100

    first program

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -1,5 +1,7 @@
 public class FirstProgram {
     public static void main(String[] args){
+        //single line comment
+        /* multi-line comment */
         System.out.println("Another java program incoming...");
     }
 }
