e76ce014a66e0657c9e01025dac4693a1e03d573
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-21 17:06:19 +0100

    operators

--- src/Operators.java
+++ src/Operators.java
@@ -5,5 +5,16 @@ public class Operators {
         int number = 10;
         String game = "Ludo";
 
+        // arithmetic operators
+        int a = 1;
+        int b = 2;
+
+        System.out.println(a + b);
+        System.out.println(a - b);
+        System.out.println(a * b);
+        System.out.println(b / a);
+        System.out.println(a % b);
+
+
     }
 }
