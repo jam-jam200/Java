e409fe24164a99673bddfc0b5d252a0218fdb650
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-21 18:03:56 +0100

    if else statements

--- src/IfElseStatement.java
+++ src/IfElseStatement.java
@@ -1,5 +1,13 @@
 public class IfElseStatement {
     public static void main (String[] args){
+        // if(expression){ statement} else{statement}
+
+        int a = 10;
+        int b = 20;
+
+        if (a > b) {
+            System.out.println("A is greater than b");
+        }
 
     }
 }
