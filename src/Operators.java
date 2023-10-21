eab73636cac3105d1759e0f4667ed5d4d5ec4b92
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-21 17:28:10 +0100

    operators

--- src/Operators.java
+++ src/Operators.java
@@ -11,6 +11,14 @@ public class Operators {
         /*
         post increment: number++; increments the final calculation value by adding one
         post decrement: number--; decrements the final calculation value by subtracting one
+
+        ++i is called the pre-increment operator. When the pre-increment operator is used,
+        the value of the variable is incremented and then used.
+        For example, if the variable i is equal to 5, the statement j = ++i would increment the value of i to 6
+        and then set the value of j to 6.
+
+        In other words, the post-increment operator returns the original value of the variable,
+        while the pre-increment operator returns the incremented value of the variable.
         **/
 
         System.out.println(a + number);
