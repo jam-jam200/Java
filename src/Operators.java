fa27d9a4d21b6ec48da5f03f65529291165502a8
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-21 17:22:00 +0100

    operators

--- src/Operators.java
+++ src/Operators.java
@@ -8,8 +8,12 @@ public class Operators {
         // arithmetic operators
         int a = 1;
         int b = 2;
+        /*
+        number++; increments the original value(10) by adding 1 before it is used to calculate
+        number--; decrements the original value(10) by subtracting 1 before it is used to calculate
+        **/
 
-        System.out.println(a + b);
+        System.out.println(a + number);
         System.out.println(a - b);
         System.out.println(a * b);
         System.out.println(b / a);
