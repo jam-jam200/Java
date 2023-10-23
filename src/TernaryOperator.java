f6ec7cd8f9f130241c57a016fb26561a759162e6
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-23 16:21:30 +0100

    ternary operators

--- src/TernaryOperator.java
+++ src/TernaryOperator.java
@@ -1,5 +1,8 @@
 public class TernaryOperator {
     public static void main(String[] args){
+        // variable = (condition) ? TrueExpression : FalseExpression
 
+        int num1 = 3;
+        int num2 = 6;
     }
 }
