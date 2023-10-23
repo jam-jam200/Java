def35ea67cf2ca9224871c9597f64f520bf0f3dd
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-23 20:25:50 +0100

    ternary operators

--- src/SwitchCase.java
+++ src/SwitchCase.java
@@ -1,7 +1,8 @@
+import java.util.Scanner;
 public class SwitchCase {
     public static void main(String[] args){
         /*
-        * switch(expression):
+        * switch(expression){
         *   case 1:
         *       statements;
         *   case 2:
@@ -11,8 +12,20 @@ public class SwitchCase {
         *
         *   default:
         *       statement;
-        *
+        * }
         */
+
+        Scanner scanner = new Scanner(System.in);
+        int day = scanner.nextInt();
+
+        switch (day){
+            case 1:
+                System.out.println("Sunday");
+                break;
+            case 1:
+                System.out.println("Sunday");
+                break;
+        }
 
 
     }
