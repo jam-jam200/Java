2578cf96e6021460310788482b02d4d42ee0edc6
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-23 22:22:36 +0100

    break and continue

--- src/BreakAndContinue.java
+++ src/BreakAndContinue.java
@@ -2,8 +2,13 @@ public class BreakAndContinue {
     public static void main(String[] args){
 
         for (int i = 1; i <= 10; i++){
+
+            if (i == 7) {
+                break;
+            }
             System.out.println(i);
         }
+        System.out.println("Outside loop, final statement");
 
     }
 }
