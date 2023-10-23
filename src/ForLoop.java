9ebb0a094c3e24b6c259735d1a9c7b216479d080
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-23 21:20:57 +0100

    for loop

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -1,5 +1,9 @@
 public class ForLoop {
     public static void main(String[] args){
         // for(initialization; condition; increment/decrement){ statement}
+
+        for (int i = 2; i < 10; i++){
+            System.out.println(i);
+        }
     }
 }
