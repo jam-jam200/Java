bbdb90a5ad0f1a0e8c84ceaf2265d8701d96068e
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-23 20:45:40 +0100

    ternary operators

--- src/Loops.java
+++ src/Loops.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 public class Loops {
     public static void main(String[] args){
         /*
@@ -5,5 +6,12 @@ public class Loops {
         *   statement
         * }
         */
+
+        System.out.println("Pick a number between 1-7");
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+
+
+
     }
 }
