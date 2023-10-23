0b43913a85b54a8a37583736596307aac8040673
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-23 16:44:47 +0100

    ternary operators

---
+++ src/SwitchCase.java
@@ -0,0 +1,17 @@
+public class SwitchCase {
+    public static void main(String[] args){
+        /*
+        * switch(expression):
+        *   case 1:
+        *       statements;
+        *   case 2:
+        *       statements;
+        *
+        *   ...
+        *
+        *   default:
+        *       statement;
+        *
+        */
+    }
+}
