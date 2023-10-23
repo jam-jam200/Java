6226ed52b47a5a0c005ae4f852e5bab46a168bbf
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-23 20:27:36 +0100

    ternary operators

--- src/SwitchCase.java
+++ src/SwitchCase.java
@@ -22,23 +22,23 @@ public class SwitchCase {
             case 1:
                 System.out.println("Sunday");
                 break;
-            case 1:
-                System.out.println("Sunday");
+            case 2:
+                System.out.println("Monday");
                 break;
-            case 1:
-                System.out.println("Sunday");
+            case 3:
+                System.out.println("Tuesday");
                 break;
-            case 1:
-                System.out.println("Sunday");
+            case 4:
+                System.out.println("Wednesday");
                 break;
-            case 1:
-                System.out.println("Sunday");
+            case 5:
+                System.out.println("Thursday");
                 break;
-            case 1:
-                System.out.println("Sunday");
+            case 6:
+                System.out.println("Friday");
                 break;
-            case 1:
-                System.out.println("Sunday");
+            case 7:
+                System.out.println("Saturday");
                 break;
         }
 
