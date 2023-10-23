d0a27fddd4bf4dc9a1abcf3445130c4d7ed99859
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-23 20:50:28 +0100

    ternary operators

--- src/Loops.java
+++ src/Loops.java
@@ -7,11 +7,13 @@ public class Loops {
         * }
         */
 
-        System.out.println("Pick a number between 1-7");
+        System.out.println("Enter a number");
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
 
-
+        while(num <= 20) {
+            System.out.println(num++);
+        }
 
     }
 }
