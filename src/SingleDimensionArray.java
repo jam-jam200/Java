3cdd33e92647b20a8f1b86221c1aba3888ca0a82
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-25 13:03:40 +0100

    single dimensional array

--- src/SingleDimensionArray.java
+++ src/SingleDimensionArray.java
@@ -1,7 +1,18 @@
 public class SingleDimensionArray {
     public static void main(String[] args){
+        // two ways of doing this, method 1:
+        int[] myIntArray = new int[5];
+        myIntArray[0] = 10;
+        myIntArray[1] = 20;
+        myIntArray[2] = 30;
+        myIntArray[3] = 40;
+        myIntArray[4] = 50;
 
-        int[] myArray = new int[5];
+        // method 2
+        int[] myIntArray1 = {10, 20, 30, 40, 50};
+
+
+
 
 
     }
