43c19a656b961a7bb1ae067baefadd64db71c6ab
jam-jam200 <jessieoyaks4j@gmail.com>
2023-10-25 12:49:15 +0100

    nested loop

--- src/NestedLoop.java
+++ src/NestedLoop.java
@@ -6,11 +6,12 @@ public class NestedLoop {
 
             int j = 1;
             while(j <= 5) {
-                j++;
 
                 for (int x=1; x <= 2; x++){
                     System.out.println("value of i is: " + i + " value of j is: " + j);
                 }
+                j++;
+
                 count++;
             }
         }
