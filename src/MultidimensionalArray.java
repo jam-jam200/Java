40675927448066beb3e2d2ca8b4ea11cb399ac29
jam-jam200 <jessieoyaks4j@gmail.com>
2023-11-08 16:47:43 +0100

    multi dimensional array

--- src/MultidimensionalArray.java
+++ src/MultidimensionalArray.java
@@ -2,5 +2,7 @@ public class MultidimensionalArray {
     public static void main(String[] args){
 
         int [][] multiArrray = new int[2][3];
+
+        int[][] multiArray1 = {{}, {}};
     }
 }
