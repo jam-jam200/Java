16455ca5e52668f9b12ee3c2bc5364c74ba13876
jam-jam200 <jessieoyaks4j@gmail.com>
2024-01-11 16:25:53 +0100

    multi dimensional array

--- src/Methods.java
+++ src/Methods.java
@@ -1,5 +1,15 @@
 public class Methods {
     public static void main(String[] args){
 
+
+    }
+    public String doIt() {
+        return "Yes I'll do it";
+    }
+    public Char sayNo() {
+        return "Y";
+    }
+    public Int num() {
+        return 10;
     }
 }
