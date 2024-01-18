ddb38899789be2f8c5d51d80cdb5ca847b21472f
jam-jam200 <jessieoyaks4j@gmail.com>
2024-01-18 14:51:44 +0100

    methods

--- src/testJava/exampleDemo.java
+++ src/testJava/exampleDemo.java
@@ -1,7 +1,12 @@
 package testJava;
 
+import com.example.Variables;
+
 public class exampleDemo {
     public static void main(String[] args){
+
+        Variables var = new Variables();
+        var.text();
 
     }
 
