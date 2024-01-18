9f53d37dcf0f6823c641a22f3e1ae019f538d07a
jam-jam200 <jessieoyaks4j@gmail.com>
2024-01-18 15:49:37 +0100

    methods

--- src/accessModifier/ClassA.java
+++ src/accessModifier/ClassA.java
@@ -10,4 +10,8 @@ public class ClassA {
     public void publicMethod(){
         System.out.println("Public Method");
     }
+
+    void defaultMethod(){
+        System.out.println("Default Method");
+    }
 }
