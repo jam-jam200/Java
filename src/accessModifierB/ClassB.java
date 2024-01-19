c27818b71e81a17c10feb402d690ac2e65c6e366
jam-jam200 <jessieoyaks4j@gmail.com>
2024-01-19 14:05:25 +0100

    access modifiers

--- src/accessModifierB/ClassB.java
+++ src/accessModifierB/ClassB.java
@@ -10,5 +10,7 @@ public class ClassB {
         ClassAA caa = new ClassAA();
         caa.methodAA();
         //at class level either the class is public or it's a default modifier
+        System.out.println("___________New methods___________");
+        System.out.println(ca.addition(2,3));
     }
 }
