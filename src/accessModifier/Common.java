dc682bc7f03ae27bc826825302926252ba78209b
jam-jam200 <jessieoyaks4j@gmail.com>
2024-01-19 15:31:45 +0100

    access modifiers

--- src/accessModifier/Common.java
+++ src/accessModifier/Common.java
@@ -2,9 +2,9 @@ package accessModifier;
 
 public class Common {
 
-    int length;
-    int width;
-    int height;
+    public int length;
+    public int width;
+    public int height;
 
     public void setBoxDimension(int l, int b, int h) {
         System.out.println("Dimension box created with dimensions: " + l + " length " + b + " breadth " + h + " height ");
