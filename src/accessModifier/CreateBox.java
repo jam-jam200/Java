34f0959747f6b9de312404e617a7cb8b509aa7e4
jam-jam200 <jessieoyaks4j@gmail.com>
2024-01-23 15:01:45 +0100

    access modifiers

--- src/accessModifier/CreateBox.java
+++ src/accessModifier/CreateBox.java
@@ -4,6 +4,10 @@ public class CreateBox {
     //Encapsulation is the wrapping up data under a single unit e.g. medicine capsules
 
     public static void main(String[] args){
-
+        Common cm = new Common();
+        int l = cm.height = 10;
+        int b = cm.length = 20;
+        int h = cm.width = 30;
+        cm.setBoxDimension(l, b, h);
     }
 }
