f07f01aa0fd30cfe4b16817b62f7c3c28f3635bd
jam-jam200 <jessieoyaks4j@gmail.com>
2024-01-23 16:09:26 +0100

    access modifiers

--- src/inheritance/VehicleCommon.java
+++ src/inheritance/VehicleCommon.java
@@ -2,4 +2,12 @@ package inheritance;
 
 public class VehicleCommon {
 
+    public void Start(){
+        System.out.println("Car Started...");
+    }
+
+    public void Stop(){
+        System.out.println("Car stopped...");
+    }
+
 }
