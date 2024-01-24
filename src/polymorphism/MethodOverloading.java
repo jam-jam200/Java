24bcf20a581130ec1f520f84eb16ec2488038e77
jam-jam200 <jessieoyaks4j@gmail.com>
2024-01-24 15:28:35 +0100

    polymorphism

--- src/polymorphism/MethodOverloading.java
+++ src/polymorphism/MethodOverloading.java
@@ -1,11 +1,13 @@
 package polymorphism;
 
 public class MethodOverloading {
+    //method overloading is basically same method name but different method signature
+
     public static void main(String[] args){
 
     }
 
     public void Login(String s, String p){
-
+        System.out.println("Login successful, using username and password");
     }
 }
