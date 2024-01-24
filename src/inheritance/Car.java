78629cfee3daed2ec9fe1f5f8c141303e385042d
jam-jam200 <jessieoyaks4j@gmail.com>
2024-01-24 15:14:30 +0100

    inheritance

---
+++ src/inheritance/Car.java
@@ -0,0 +1,10 @@
+package inheritance;
+
+public class Car extends VehicleCommon{
+    public static void main(String[] args){
+        Car car = new Car();
+        car.Start();
+        car.Stop();
+
+    }
+}
