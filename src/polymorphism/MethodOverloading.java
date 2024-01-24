92855ebdf04dc865545980b2d4e60902856f01bc
jam-jam200 <jessieoyaks4j@gmail.com>
2024-01-24 15:44:52 +0100

    polymorphism

--- src/polymorphism/MethodOverloading.java
+++ src/polymorphism/MethodOverloading.java
@@ -2,6 +2,7 @@ package polymorphism;
 
 public class MethodOverloading {
     //method overloading is basically same method name but different method signature
+    //The method signature is the parameter of the method
 
     public static void main(String[] args){
 
@@ -10,4 +11,6 @@ public class MethodOverloading {
     public void Login(String s, String p){
         System.out.println("Login successful, using username and password");
     }
+
+    public
 }
