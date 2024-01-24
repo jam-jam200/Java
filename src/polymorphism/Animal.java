551ab11103b446bc192cdf497ad6b5eff592d400
jam-jam200 <jessieoyaks4j@gmail.com>
2024-01-24 16:19:03 +0100

    polymorphism

---
+++ src/polymorphism/Animal.java
@@ -0,0 +1,12 @@
+package polymorphism;
+
+public class Animal {
+    public static void main(String[] args){
+
+    }
+
+    public void makeNoise(){
+        System.out.println("Animals make noise");
+    }
+
+}
