eb2177ffe44f4f3a368606e20673aff03a5bfd93
jam-jam200 <jessieoyaks4j@gmail.com>
2024-02-07 16:23:41 +0100

    abstraction

---
+++ src/abstraction/Circle.java
@@ -0,0 +1,19 @@
+package abstraction;
+
+public class Circle extends Shapes{
+
+    @Override
+    public void drawShape() {
+
+    }
+
+    @Override
+    public void colorShape() {
+
+    }
+
+    @Override
+    public void moveShape() {
+
+    }
+}
