ea7f00d85924432fb86cc106430b2af5e1d82c6f
jam-jam200 <jessieoyaks4j@gmail.com>
2024-02-07 16:27:09 +0100

    abstraction

--- src/abstraction/Shapes.java
+++ src/abstraction/Shapes.java
@@ -2,7 +2,9 @@ package abstraction;
 
 public abstract class Shapes {
 
-    //
+    //Abstraction in computer science is like simplifying things by
+    // focusing on what's important and ignoring the less important details.
+    // It helps in making programs and systems easier to understand and manage.
 
     public abstract void drawShape();
 
