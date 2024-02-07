737b4254e80d04596ee831f73d244163ee0eb934
jam-jam200 <jessieoyaks4j@gmail.com>
2024-02-07 16:11:44 +0100

    abstraction

---
+++ src/abstraction/Shapes.java
@@ -0,0 +1,16 @@
+package abstraction;
+
+public class Shapes {
+
+    public void drawShape() {
+        System.out.println("Drawing shape...");
+    }
+
+    public void colorShape(){
+        System.out.println("Coloring shape...");
+    }
+
+    public void moveShape(){
+        System.out.println("Shape is moving...");
+    }
+}
