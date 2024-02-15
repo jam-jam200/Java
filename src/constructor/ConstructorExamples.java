89347ea23832b4fc16d3780d6c7b8ae5419ab9fb
jam-jam200 <jessieoyaks4j@gmail.com>
2024-02-15 20:23:52 +0100

    constructor

--- src/constructor/ConstructorExamples.java
+++ src/constructor/ConstructorExamples.java
@@ -2,8 +2,11 @@ package constructor;
 
 public class ConstructorExamples {
     public static void main(String[] args) {
+        //default constructors are the ones called implicitly like the ones used in objects
+        //
         //
         ConstructorExamples ce = new ConstructorExamples();
+
 
     }
 }
