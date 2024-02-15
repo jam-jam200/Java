e710895c4dc21bc3bda754ae6ecd2508ea4fa06d
jam-jam200 <jessieoyaks4j@gmail.com>
2024-02-15 20:04:41 +0100

    abstraction

--- src/inter/Circle.java
+++ src/inter/Circle.java
@@ -1,18 +1,26 @@
 package inter;
 
 public class Circle implements Shapes{
+
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Circle c = new Circle();
+        c.drawShape();
+        c.clone();
+        c.colorShape();
+        c.moveShape();
+    }
     @Override
     public void drawShape() {
-
+        System.out.println("Draw circle");
     }
 
     @Override
     public void colorShape() {
-
+        System.out.println("Color circle");
     }
 
     @Override
     public void moveShape() {
-
+        System.out.println("Move circle");
     }
 }
