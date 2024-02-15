440f05b7ce84553bb55c539ec189c7aaa99f5e81
jam-jam200 <jessieoyaks4j@gmail.com>
2024-02-15 20:24:32 +0100

    constructor

--- src/constructor/ConstructorExamples.java
+++ src/constructor/ConstructorExamples.java
@@ -2,8 +2,8 @@ package constructor;
 
 public class ConstructorExamples {
     public static void main(String[] args) {
-        //default constructors are the ones called implicitly like the ones used in objects
-        //
+        // default constructors are the ones called implicitly like the ones used in objects
+        // the other types are argument and parameterized constructors
         //
         ConstructorExamples ce = new ConstructorExamples();
 
