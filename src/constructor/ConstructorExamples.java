ccd010f2a782207e3d7dcb53974db03ee121321b
jam-jam200 <jessieoyaks4j@gmail.com>
2024-02-15 21:35:42 +0100

    constructor

--- src/constructor/ConstructorExamples.java
+++ src/constructor/ConstructorExamples.java
@@ -1,13 +1,32 @@
 package constructor;
 
 public class ConstructorExamples {
+
+    public ConstructorExamples() {
+        System.out.println("no argument constructor");
+    }
+
+    public ConstructorExamples(int i) {
+        System.out.println("integer constructor");
+    }
+
+    public ConstructorExamples(int i, int y) {
+        System.out.println("2 integer constructor");
+    }
+
+
+
+
     public static void main(String[] args) {
         // default constructors are the ones called implicitly like the ones used in objects
         // the other types are no-argument and parameterized constructors
         // explicitly trying to define a constructor, and you don't want to call the constructor implicitly
         // you create a constructor with the same name as the class, it'll be a no-argument constructor
         // there's a no return type
-        ConstructorExamples ce = new ConstructorExamples();
+       // ConstructorExamples ce = new ConstructorExamples();
+
+
+        }
 
 
     }
