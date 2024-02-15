fbcc94ddb498fcd83b9cc5e8d718a081b38724f6
jam-jam200 <jessieoyaks4j@gmail.com>
2024-02-15 20:02:05 +0100

    abstraction

---
+++ src/inter/Circle.java
@@ -0,0 +1,18 @@
+package inter;
+
+public class Circle implements Shapes{
+    @Override
+    public void drawShape() {
+
+    }
+
+    @Override
+    public void colorShape() {
+
+    }
+
+    @Override
+    public void moveShape() {
+
+    }
+}
