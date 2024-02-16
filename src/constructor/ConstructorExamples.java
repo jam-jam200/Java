960d83a5537dd8bc26482f32077e79d395d3ac03
jam-jam200 <jessieoyaks4j@gmail.com>
2024-02-16 19:05:26 +0100

    constructor

--- src/constructor/ConstructorExamples.java
+++ src/constructor/ConstructorExamples.java
@@ -7,7 +7,7 @@ public class ConstructorExamples {
     }
 
     public ConstructorExamples(int i) {
-        System.out.println("integer constructor in progress");
+        System.out.println("integer constructor in progr");
     }
 
     public ConstructorExamples(int i, int y) {
